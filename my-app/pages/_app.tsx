33d02c4593dfae774ca8265397935801bfcd6bdf
Jamie Hau <yshau23@outlook.com>
2025-07-25 14:44:12 +0800

    Create basic overall website layout

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,9 +1,12 @@
+// pages/_app.tsx
+import '../styles/globals.css'; // Make sure this path is correct for your Tailwind setup
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';  
-import Layout from '../components/layout';
+import Layout from '../components/layout'; // Import your Layout component
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    // Your Layout component wraps the current page component (Component).
+    // The content of 'Component' will be passed as 'children' to 'Layout'.
     <Layout>
       <Component {...pageProps} />
     </Layout>
